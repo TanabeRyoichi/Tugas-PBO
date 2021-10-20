afd2b2c8c7682b3652149ff7214f0e10cc1912cf
TanabeRyoichi <71366913+TanabeRyoichi@users.noreply.github.com>
2021-10-20 19:24:44 +0700

    Add files via upload

---
+++ Tugas 4/Buku.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Buku {
+    private String judul;
+    private String pengarang;
+    private  double harga;
+
+    public  Buku(String judul,String pengarang,double harga){
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.harga = harga;
+    }
+    public String getJudul(){
+        return  judul;
+    }
+    public void setJudul(String judul){
+        this.judul = judul;
+    }
+    public double getHarga(){
+        return  harga;
+    }
+    public void setHarga(double harga){
+        this.harga= harga;
+    }
+    public String getPengarang(){
+        return  pengarang;
+    }
+    public void setPengarang(double harga){
+        this.pengarang= pengarang;
+    }
+
+}
